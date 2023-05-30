8eef534a2681d85e8cec6961a91fd21c36898dc8
MaryKuzo <124557630+MaryKuzo@users.noreply.github.com>
2023-05-30 13:43:29 +0300

    task-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const TIMER_DELAY = 500;
+let timerId = null;
+const refs = {
+  onStartBtn: document.querySelector('button[data-start]'),
+  onStopBtn: document.querySelector('button[data-stop]'),
+  body : document.querySelector('body')
+};
+refs.onStopBtn.disabled = true;
+refs.onStartBtn.addEventListener('click', onStartBtnChangeColor);
+refs.onStopBtn.addEventListener('click', onStopBtnChangeColor);
 
+function onStartBtnChangeColor() {
+  refs.onStartBtn.disabled = true;
+  refs.onStopBtn.disabled = false;
+  timerId = setInterval(() => { refs.body.style.backgroundColor = getRandomHexColor() }, TIMER_DELAY);
+}
+function onStopBtnChangeColor() {
+  refs.onStopBtn.disabled = true;
+  refs.onStartBtn.disabled = false;
+  clearInterval(timerId)
+
+}
+
+
+
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
